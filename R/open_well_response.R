7277ff50c66a06727bb1519d654adbd61fb31d23
Andy Barbour <andy.barbour@gmail.com>
2013-07-31 19:22:43 -0700

    Adding Hsieh model... not quite there yet.  Have to double check both the formulation and my execution.

--- R/open_well_response.R
+++ R/open_well_response.R
@@ -26,8 +26,11 @@
 #' @param omega  numeric; frequency,  (see \code{freq.units})
 #' @param T. numeric; effective aquifer transmissivity \eqn{[m^2/s]}
 #' @param S. numeric; well storativity,  \eqn{[unitless]}
-#' @param z. numeric; the
-#' @param freq.units charachter; setup the units of \code{omega}
+#' @param z. numeric; From Rojstaczer (1988): 'the depth from the water table'
+#' @param Rs. numeric; the \emph{radius} of the open (screened) section
+#' @param rho numeric; fluid density (assumed if missing)
+#' @param grav numeric; the local gravitational acceleration (assumed if missing)
+#' @param freq.units character; setup the units of \code{omega}
 #' @param model  character; setup the response model from 
 #'    Rojstaczer (1988),
 #'    Liu et al (1989), or 
@@ -44,40 +47,47 @@
 #' @seealso \code{\link{well_response}}, and
 #' \code{\link{kitagawa-package}} for references and more background.
 #'
-open_well_response <- function(omega, T., S., z.,
+open_well_response <- function(omega, T., S., z., 
+                               Rs.=(8/12)*(1200/3937),
+                               rho, grav,
                                freq.units=c("rad_per_sec","Hz"),
                                model=c("roj","liu","cooper")) UseMethod("open_well_response")
 #' @rdname open_well_response
 #' @method open_well_response default
 #' @S3method open_well_response default
 open_well_response.default <- function(omega, T., S., z.,
+                                       Rs.=(8/12)*(1200/3937),
+                                       rho, grav,
                                        freq.units=c("rad_per_sec","Hz"),
-                                       model=c("rojstaczer","liu","cooper")){
+                                       model=c("rojstaczer","liu","cooper","hsieh")){
   model <- match.arg(model)
   # Enforce units of omega to be radians/sec
   fc <- switch(match.arg(freq.units), rad_per_sec=1, Hz=2*pi)
   omega <- fc*omega
   #
   const <- kitagawa:::constants(FALSE)
-  rho <- const$water$density
-  grav <- const$gravity
+  if (missing(rho)) rho <- const$water$density
+  if (missing(grav)) grav <- const$gravity
   rhog <- rho*grav
   #
+  Dtau. <- omega_constants(omega, c.type="diffusivity_time", S.=S., T.=T.)
+  # Dtau == sqrt( omega / 2 / D.) == sqrt (omega * S / 2 / T)
+  # sqrt Qp <- z. * sqrt(omega / 2 / D) = z * Dtau
+  #
   if (model=="rojstaczer"){
-    Dtau. <- omega_constants(omega, c.type="diffusivity_time", S.=S., T.=T.)
-    # Dtau == sqrt( omega / 2 / D.)
-    # sqrt Qp <- z. * sqrt(omega / 2 / D) = z * Dtau
     sQp <- z. * Dtau.
     exptau <- exp(-sQp)
-    A. <- rhog(exptau*cos(sQp) - 1)
+    # scale by rhog for pressure over strain relative to static response
+    A. <- rhog*(exptau*cos(sQp) - 1)
     B. <- -1*rhog*exptau*sin(sQp)
     wellresp <- complex(real=A., imaginary=B.)
-  } else if (model=="liu" | model=="cooper"){
-    
+  } else if (model %in% c("liu","cooper","hsieh")){
     # constants alpha and Kel
     Alpha. <- omega_constants(omega, c.type="alpha", S.=S., T.=T., Rs.=Rs.)
-    Kmat <- alpha_constants(Alpha., c.type="Kel")
-    Kel0. <- Kmat[,2] # zero order Kelvin function (complex)
+    KelPhiPsi <- alpha_constants(Alpha., c.type="Phi") #alpha, kel0, kel1, phi, psi
+    Kel0. <- KelPhiPsi[,2] # zero order Kelvin function (complex)
+    Phi. <- KelPhiPsi[,4] # zero order Kelvin function (complex)
+    Psi. <- KelPhiPsi[,5] # zero order Kelvin function (complex)
     kei. <- Im(Kel0.)
     ker. <- Re(Kel0.)
     omegsq <- omega**2 # used a few times
@@ -100,6 +110,15 @@ open_well_response.default <- function(omega, T., S., z.,
       B. <- complex(imaginary=omega*U.*Rs.**2 * gamma*expgam/(1-exp2gam))
       wellresp <- 1/(A. - B. + 1)
       #
+    } else if (model=="hsieh"){
+      #
+      # R = rc**2 * omega / 2 T
+      R. <- Rs.**2 * (Dtau.**2 / S.)
+      U1. <- Psi. * ker.  +  Phi. * kei.
+      U2. <- Phi. * ker.  +  Psi. * kei.
+      A. <- 1  -  R. * U1.
+      B. <- -1 * R. * U2.
+      wellresp <- complex(real=A., imaginary=B.)
     } else if (model=="cooper"){
       .NotYetImplemented()
       #
@@ -117,7 +136,8 @@ open_well_response.default <- function(omega, T., S., z.,
       # h/e_kk
       # h/e_areal ?
       # wellresp <- wellresp * -2 * mu * B. * (1+nu_u) /(rho. * grav * 3 * (1-2 * nu_u)) # | w/h * h/e_kk|
-      omega_peak <- sqrt(grav./He.) # eq 21
+      #
+      ##omega_peak <- sqrt(grav./He.) # eq 21
     }
   }
   omega <- omega/fc
